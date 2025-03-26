1ca3ce969e9c093e1397b43da3d90e200d98e058
rvsfr <rvsfr@VIKA>
2025-03-26 16:23:34 +0300

    register user test case without refactoring

--- TestProject/HomePageAutoTests.cs
+++ TestProject/HomePageAutoTests.cs
@@ -43,16 +43,100 @@ public class HomePageAutoTests
         var nameInput = _driver.FindElement(By.XPath("//input[@data-qa='signup-name']"));
         var emailInput = _driver.FindElement(By.XPath("//input[@data-qa='signup-email']"));
     
-        nameInput.SendKeys("VR");
-        emailInput.SendKeys("vrcsad@gmail.com");
+        var timestamp = $"{DateTime.Now:yyyyMMddHHmmss}";
+        var username = $"TestUser - {timestamp}";
+        
+        nameInput.SendKeys(username);
+        emailInput.SendKeys($"test{timestamp}@example.com");
     
         // 7. Click 'Signup' button
         var signUpButton = _driver.FindElement(By.XPath("//button[@data-qa='signup-button']"));
         signUpButton.Click();
     
         // 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
-        var enterAccountInformation = _driver.FindElement(By.XPath("//h2[contains(., 'Enter Account Information')]"));
-        enterAccountInformation.Displayed.Should().BeTrue();
+        var accountInfoHeader = _driver.FindElement(By.XPath("//h2[contains(., 'Enter Account Information')]"));
+        accountInfoHeader.Displayed.Should().BeTrue();
+        
+        // 9. Fill details: Title, Name, Email, Password, Date of birth
+        var accountInformationTitle = _driver.FindElement(By.XPath("//input[@id='id_gender2']"));
+        accountInformationTitle.Click();
+        
+        var accountInformationPassword = _driver.FindElement(By.XPath("//input[@id='password']"));
+        accountInformationPassword.SendKeys("Test1234");
+        
+        var birthDateDaysDropdown = _driver.FindElement(By.XPath("//select[@id='days']"));
+        birthDateDaysDropdown.Click();
+        birthDateDaysDropdown.FindElement(By.XPath(".//option[@value='15']")).Click();
+        
+        var birthDateMonthDropdown = _driver.FindElement(By.XPath("//select[@id='months']"));
+        birthDateMonthDropdown.Click();
+        birthDateMonthDropdown.FindElement(By.XPath(".//option[@value='5']")).Click();
+        
+        var birthDateYearDropdown = _driver.FindElement(By.XPath("//select[@id='years']"));
+        birthDateYearDropdown.Click();
+        birthDateYearDropdown.FindElement(By.XPath(".//option[@value='1990']")).Click();
+        
+        // 10. Select checkbox 'Sign up for our newsletter!'
+        var newsletterCheckbox  = _driver.FindElement(By.XPath("//input[@id='newsletter']"));
+        newsletterCheckbox.Click();
+        
+        // 11. Select checkbox 'Receive special offers from our partners!'
+        var specialOffersCheckbox   = _driver.FindElement(By.XPath("//input[@id='optin']"));
+        specialOffersCheckbox.Click();
+        
+        // 12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+        
+        var accountInformationFirstName = _driver.FindElement(By.XPath("//input[@data-qa='first_name']"));
+        var accountInformationLastName = _driver.FindElement(By.XPath("//input[@data-qa='last_name']"));
+        var accountInformationCompany = _driver.FindElement(By.XPath("//input[@data-qa='company']"));
+        var accountInformationAddress = _driver.FindElement(By.XPath("//input[@data-qa='address']"));
+        var accountInformationAddress2 = _driver.FindElement(By.XPath("//input[@data-qa='address2']"));
+        var accountInformationCountryDropdown = _driver.FindElement(By.XPath("//select[@id='country']"));
+        var accountInformationState = _driver.FindElement(By.XPath("//input[@data-qa='state']"));
+        var accountInformationCity = _driver.FindElement(By.XPath("//input[@data-qa='city']"));
+        var accountInformationZipCode = _driver.FindElement(By.XPath("//input[@data-qa='zipcode']"));
+        var accountInformationMobileNumber = _driver.FindElement(By.XPath("//input[@data-qa='mobile_number']"));
+        
+        accountInformationFirstName.SendKeys("Test");
+        accountInformationLastName.SendKeys("User");
+        accountInformationCompany.SendKeys("TestCompany");
+        accountInformationAddress.SendKeys("123 Test Street");
+        accountInformationAddress2.SendKeys("TApt 4B");
+        
+        accountInformationCountryDropdown.Click();
+        accountInformationCountryDropdown.FindElement(By.XPath(".//option[contains(text(),'United States')]")).Click();
+        
+        accountInformationState.SendKeys("California");
+        accountInformationCity.SendKeys("Los Angeles");
+        accountInformationZipCode.SendKeys("90001");
+        accountInformationMobileNumber.SendKeys("1234567890");
+
+        // 13. Click 'Create Account button'
+        var createAccountButton = _driver.FindElement(By.XPath("//button[@data-qa='create-account']"));
+        createAccountButton.Click();
+        
+        // 14. Verify that 'ACCOUNT CREATED!' is visible
+        var accountCreatedHeader = _driver.FindElement(By.XPath("//h2[@data-qa='account-created']"));
+        accountCreatedHeader.Text.Should().Be("ACCOUNT CREATED!");
+        
+        // 15. Click 'Continue' button
+        var continueButton = _driver.FindElement(By.XPath("//a[@data-qa='continue-button']"));
+        continueButton.Click();
+        
+        // 16. Verify that 'Logged in as username' is visible
+        var loggedInText = _driver.FindElement(By.XPath("//li[10]/a"));
+        loggedInText.Text.Should().Be($"Logged in as {username}");
+        
+        // 17. Click 'Delete Account' button
+        var deleteAccountButton = _driver.FindElement(By.XPath("//a[contains(@href,'/delete_account')]"));
+        deleteAccountButton.Click();
+
+        // 18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+        var accountDeletedHeader = _driver.FindElement(By.XPath("//h2[@data-qa='account-deleted']"));
+        accountDeletedHeader.Text.Should().Be("ACCOUNT DELETED!");
+        
+        continueButton = _driver.FindElement(By.XPath("//a[@data-qa='continue-button']"));
+        continueButton.Click();
     }
 
     [TearDown]
